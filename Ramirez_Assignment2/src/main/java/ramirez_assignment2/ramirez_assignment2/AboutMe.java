6e73e1f83b899e63cffd9a5f84c4e2e629102559
angelrami58 <114256627+angelrami58@users.noreply.github.com>
2023-12-11 15:30:44 -0600

    Add files via upload

---
+++ Ramirez_Assignment2/src/main/java/ramirez_assignment2/ramirez_assignment2/AboutMe.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ramirez_assignment2.ramirez_assignment2;
+/**
+ *
+ * @author angelramirez-rivera
+ */
+public class AboutMe {
+    public AboutMe() {
+         System.out.println(java.time.LocalTime.now());
+         System.out.println("Angel Ramirez-Rivera\tAssignment 2");
+         System.out.println("ITDEV-110\t\tIntro to Oject Oriented Programming");
+         System.out.println("J.Christie\t\t01\\30\\2023");
+    }
+}
---
+++ Ramirez_Assignment2/src/main/java/ramirez_assignment2/ramirez_assignment2/Ramirez_Assignment2.java
@@ -0,0 +1,19 @@
+/*
+* Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ramirez_assignment2.ramirez_assignment2;
+
+/**
+ *
+ * @author angelramirez-rivera
+ * Angel Ramirez-Rivera     Assignment 2
+ * ITDEV-110                Intro to Object Oriented Programming
+ * J.Christie               01/30/2023
+ */
+public class Ramirez_Assignment2 {
+
+    public static void main(String[] args) {
+        AboutMe Me = new AboutMe();
+    }
+}
